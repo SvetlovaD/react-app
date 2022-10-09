4577c6c540fa2a2da0c39fe0ebef133158d82a04
Daria Shut <d.svetlova29@gmail.com>
2022-10-10 02:21:46 +0300

    footer design

--- my-app/src/components/footer/footer.jsx
+++ my-app/src/components/footer/footer.jsx
@@ -4,9 +4,9 @@ import style from './footer.module.css';
 const Footer = () => {
     return (
         <div className={style.footer}>
-            Footer
+            <p className={style.bye}>Copyright ©2022 This website is coded with❤by <a href="https://github.com/SvetlovaD">Svetlova</a></p>
         </div>
     )
 }
 
-export default Footer;
+export default Footer; 
