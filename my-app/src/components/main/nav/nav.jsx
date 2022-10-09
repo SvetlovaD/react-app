058aee10d72918fd3c0eb37ea6a7f09bc0a46811
Daria Shut <d.svetlova29@gmail.com>
2022-10-10 01:56:33 +0300

    nav design updated

--- my-app/src/components/main/nav/nav.jsx
+++ my-app/src/components/main/nav/nav.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './nav.module.css';
-import profile from '../img/user.png';
-import messages from '../img/envelope.png';
-import friends from '../img/users-alt.png';
-import news from '../img/layout-fluid.png';
-import videos from '../img/play-alt.png';
+import profile from './img/user.png';
+import messages from './img/envelope.png';
+import friends from './img/users-alt.png';
+import news from './img/layout-fluid.png';
+import videos from './img/play-alt.png';
+import girl from '../img/girl.png';
+
+const Avatar = () => {
+    return (
+        <div className={style.avaWrapper}>
+            <div className={style.imgWrapper}><img src={girl} alt="" className={style.avaImg}/></div>
+            <h2>Kira Lambert</h2>
+        </div>
+    )
+}
 
 const Nav = () => {
     return (
         <div className={style.nav}>
+            <Avatar/>
+            <div className={style.itemsWrapper}>
             <div className= {style.navItems}>
                 <img src={profile} alt="" className={style.navIcons}/>
             <NavLink to="/profile">Profile</NavLink>
@@ -29,6 +41,7 @@ const Nav = () => {
             <div className= {style.navItems}>
                 <img src={videos} alt="" className={style.navIcons}/>
             <NavLink to="/settings">Videos</NavLink>
+            </div>
             </div>
         </div>
     )
